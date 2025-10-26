4201216301b3e64ff5ffcdf8706077514d7b77ac
Deepak Bhole <bholedeepak07@gmail.com>
2025-10-26 20:49:16 +0530

    permission for storage fixed

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'chalrahiStorage',
   access: (allow) => ({
-    'profilePictures/{cognitoSub}/*': [
+    'profilePictures/{entity_id}/*': [
       allow.entity('identity').to(['read', 'write', 'delete']),
       allow.authenticated.to(['read'])
     ],
