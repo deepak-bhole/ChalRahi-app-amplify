f3cd26377da25181a38ce16a8ec8004afbdecf66
Deepak Bhole <bholedeepak07@gmail.com>
2025-10-27 16:50:48 +0530

    reverted back add owners

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -52,14 +52,12 @@ const schema = a.schema({
     club: a.belongsTo('Club', 'clubId'),
     user: a.belongsTo('User', 'userId'),
     role: a.enum(['MEMBER', 'ADMIN']),
-    owners: a.string().array(),
 
     status: a.enum(['PENDING', 'APPROVED']),
     
   }).authorization(allow => [
-    allow.owner(), 
-    allow.ownersDefinedIn('owners').to(['update', 'delete']), 
-    allow.authenticated().to(['read', 'create'])
+    allow.owner().to(['update', 'delete']), 
+    allow.authenticated().to(['read', 'create', 'update'])
   ]),
 
   Event: a.model({
